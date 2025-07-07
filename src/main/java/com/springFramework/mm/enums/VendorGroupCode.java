81f34033c5331542073b23b057a2ce0f52ef4073
standard-chan <starp321@naver.com>
2025-07-07 15:48:42 +0900

    feat: 구매처그룹 코드 enum 구현

---
+++ src/main/java/com/springFramework/mm/enums/VendorGroupCode.java
@@ -0,0 +1,22 @@
+package com.springFramework.mm.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum VendorGroupCode {
+    DOMESTIC("1000", "국내"),
+    OVERSEAS("2000", "해외"),
+    INDIVIDUAL("3000", "개인"),
+    DEPARTMENT("4000", "부서"),
+    CORPORATE_CARD("5000", "법인카드"),
+    FINANCIAL_INSTITUTION("6000", "금융기관"),
+    ONE_TIME("9000", "1회성");
+
+    private final String code;
+    private final String description;
+
+    VendorGroupCode(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+}
