4f688c4af3c84e8f981c5f5240da887299fc037b
standard-chan <starp321@naver.com>
2025-07-07 15:35:28 +0900

    feat : Vendor Entity 추가

--- src/main/java/com/springFramework/mm/domain/Vendor.java
+++ src/main/java/com/springFramework/mm/domain/Vendor.java
@@ -1,4 +1,25 @@
 package com.springFramework.mm.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Vendor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String vendorCode; // 구매처 번호
+
+    private String name; // 구매처 이름
+
+    private String businessRegistrationNo; // 사업자 번호
+
+    private String personalId; // 개인 번호
+
+    private String vendorGroupCode; // 구매처 그룹
+
+    private String countryCode; // 구매처 그룹
+
+    private String address; // 주소
 }
