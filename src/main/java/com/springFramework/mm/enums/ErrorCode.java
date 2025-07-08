29424fc15bdd2861a7f74ff9de5a3353b9da6797
standard-chan <starp321@naver.com>
2025-07-08 15:23:51 +0900

    feat: error code Enum 작성

---
+++ src/main/java/com/springFramework/mm/enums/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.springFramework.mm.enums;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    // 400
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    MISSING_REQUIRED_VALUE(HttpStatus.BAD_REQUEST, "필수 값이 누락되었습니다."),
+
+    // 404
+    NOT_FOUND_VENDOR(HttpStatus.NOT_FOUND, "해당 벤더를 찾을 수 없습니다."),
+    NOT_FOUND_COMPANY(HttpStatus.NOT_FOUND, "해당 회사코드를 찾을 수 없습니다."),
+    NOT_FOUND_PURCHASING(HttpStatus.NOT_FOUND, "해당 구매조직을 찾을 수 없습니다."),
+
+    // 409
+    CONFLICT_RELATION_EXISTS(HttpStatus.CONFLICT, "연관관계가 존재하여 삭제할 수 없습니다."),
+
+    // 500
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
+
+
+    private final HttpStatus status;
+    private final String description;
+
+    ErrorCode(HttpStatus status, String description) {
+        this.status = status;
+        this.description = description;
+    }
+}
