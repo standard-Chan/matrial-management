5edcdd0124cd7a1b730fd0d543a74bbc41b05e59
standard-chan <starp321@naver.com>
2025-07-09 12:36:23 +0900

    feat: QuantityUnit Enum 추가

---
+++ src/main/java/com/springFramework/mm/enums/QuantityUnit.java
@@ -0,0 +1,29 @@
+package com.springframework.mm.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum QuantityUnit {
+    EACH("EA", "개"),
+    BOX("BOX", "박스"),
+    ROLL("ROLL", "롤"),
+    KG("KG", "킬로그램"),
+    G("G", "그램"),
+    MG("MG", "밀리그램"),
+    L("L", "리터"),
+    ML("ML", "밀리리터"),
+    METER("M", "미터"),
+    CM("CM", "센티미터"),
+    MM("MM", "밀리미터"),
+    SET("SET", "세트"),
+    PACK("PACK", "팩");
+
+    private final String code;
+    private final String description;
+
+    QuantityUnit(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+}
+
